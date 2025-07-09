54b0ab964921a0ca1ce30c99c3f78f888d85df96
Vinh Quang Lê <quankle2004@gmail.com>
2025-07-09 21:53:54 +0700

    change database

--- backend/database/migrations/2025_06_25_035112_create_products_table.php
+++ backend/database/migrations/2025_06_25_035112_create_products_table.php
@@ -4,25 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('image'); // ảnh đại diện chính của sản phẩm
+            $table->string('image');
             $table->string('category')->nullable();
             $table->text('description')->nullable();
+            $table->string('style')->nullable();
+            $table->string('tag')->nullable();
+            $table->boolean('is_on_top')->default(0);
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
--- backend/database/migrations/2025_06_25_035305_create_product_details_table.php
+++ backend/database/migrations/2025_06_25_035305_create_product_details_table.php
@@ -4,25 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('product_details', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constrained()->onDelete('cascade'); // liên kết với bảng products
-            $table->string('image');           // ảnh mô tả
-            $table->string('title')->nullable();       // tiêu đề ảnh
-            $table->text('description')->nullable();   // mô tả ảnh
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->string('image')->nullable();
+            $table->string('title')->nullable();
+            $table->text('intro')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('product_details');
